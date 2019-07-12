c6a5a1aa7fcde819dd503d92b08989bd2cbaac2e
fredy.vega@ingeneo.com.co <fredy.vega@ingeneo.com.co>
2019-07-12 11:49:51 -0500

    Ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params, NavigationEnd } from "@angular/router";
+import { map, filter } from 'rxjs/operators';
+
+declare var dataLayer;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Prueba Angular Sophos';
+export class AppComponent implements OnInit {
+  title = 'gp works!';
+  constructor(
+    public _router: Router,
+    public route: ActivatedRoute,
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.sendHitGoogleAnalytics();
+  }
+
+  sendHitGoogleAnalytics() {
+    this._router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map((event: NavigationEnd) => {
+        dataLayer.push({ event: 'pageview', page: { page: event.url } });
+      })
+    ).subscribe();
+  }
 }
